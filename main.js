c7f8b15f383c14f34e1d96a5d68b4fb5a87319c1
ritamrao <54271550+ritamrao@users.noreply.github.com>
2022-10-08 19:56:40 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import 'https://ritamrao.github.io/style.css'
 
-import * as THREE from 'https://ritamrao.github.io/Three.js';
+import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/0.145.0/three.min.js';
 
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
